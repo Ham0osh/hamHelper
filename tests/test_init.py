6ce0f04770b213f8620f87f0d9f40e15975f2325
Hamoosh <67180838+Ham0osh@users.noreply.github.com>
2024-03-16 23:04:27 -0700

    Update test_init.py
    fix mismatched assert

--- tests/test_init.py
+++ tests/test_init.py
@@ -6,5 +6,5 @@ def test_hello_world(capsys):
     """Test hello world function"""
     hello_world()
     captured = capsys.readouterr()
-    assert captured.out == 'This is my first pip package!\n'
+    assert captured.out == 'Found hamHelper!'
     assert captured.err == ''

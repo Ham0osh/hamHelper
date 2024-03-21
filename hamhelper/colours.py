c0e7450a116d0e6f4c91ca7ddd1e48ecd5ed5de7
hamishj <hamish_johnson@sfu.ca>
2024-03-21 16:59:05 -0700

    Incorrect rgb_to_dec conversion factor

--- hamhelper/colours.py
+++ hamhelper/colours.py
@@ -75,7 +75,7 @@ def rgb_to_dec(value):
     Converts rgb to decimal colours (i.e. divides each value by 256)
     value: list (length 3) of RGB values
     Returns: list (length 3) of decimal values'''
-    return [v/256 for v in value]
+    return [v/255 for v in value]
 
 
 def rgb_lst_to_hex(lst):
